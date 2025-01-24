bd0d8876f3b99250db39dd8ed2e1e023162275df
NoJamBean <106692414+NoJamBean@users.noreply.github.com>
2025-01-24 19:49:36 +0900

    mod

--- seoulaws1.sh
+++ seoulaws1.sh
@@ -4,8 +4,8 @@ amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd lynx
 systemctl start httpd && systemctl enable httpd
 mkdir /var/www/inc
-curl -o /var/www/inc/dbinfo.inc https://cloudneta-book.s3.ap-northeast-2.amazonaws.com/chapter8/dbinfo.inc
-curl -o /var/www/html/db.php https://raw.githubusercontent.com/NoJamBean/Result1/refs/heads/main/spidcdb.sh
+curl -o /var/www/inc/dbinfo.inc php https://raw.githubusercontent.com/NoJamBean/Result1/refs/heads/main/dbinfo.inc
+curl -o /var/www/html/db.php https://raw.githubusercontent.com/NoJamBean/Result1/refs/heads/main/seouldb.php
 rm -rf /var/www/html/index.html
 echo "<h1>SeoulRegion - Web1</h1>" > /var/www/html/index.html
 curl -o /opt/pingcheck.sh https://cloudneta-book.s3.ap-northeast-2.amazonaws.com/chapter8/pingchecker.sh
@@ -13,4 +13,4 @@ chmod +x /opt/pingcheck.sh
 cat <<EOT>> /etc/crontab
 */3 * * * * root /opt/pingcheck.sh
 EOT
-echo "1" > /var/www/html/HealthCheck.txt
+echo "1" > /var/www/html/HealthCheck.txt 
--- spaws1.sh
+++ spaws1.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-(
-echo "qwe123"
-echo "qwe123"
-) | passwd --stdin root
-sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sed -i "s/^#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
-service sshd restart
 hostnamectl --static set-hostname SP-AWS-Web1
 amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd lynx
 systemctl start httpd && systemctl enable httpd
 mkdir /var/www/inc
-curl -o /var/www/inc/dbinfo.inc https://cloudneta-book.s3.ap-northeast-2.amazonaws.com/chapter8/dbinfo.inc
-curl -o /var/www/html/db.php https://cloudneta-book.s3.ap-northeast-2.amazonaws.com/chapter8/db.php
+curl -o /var/www/inc/dbinfo.inc php https://raw.githubusercontent.com/NoJamBean/Result1/refs/heads/main/dbinfo.inc
+curl -o /var/www/html/db.php https://raw.githubusercontent.com/NoJamBean/Result1/refs/heads/main/seouldb.php
 rm -rf /var/www/html/index.html
-echo "<h1>CloudNet@ FullLab - SPRegion - Web1</h1>" > /var/www/html/index.html
+echo "<h1>SPRegion - Web1</h1>" > /var/www/html/index.html
 curl -o /opt/pingcheck.sh https://cloudneta-book.s3.ap-northeast-2.amazonaws.com/chapter8/pingchecker.sh
 chmod +x /opt/pingcheck.sh
 cat <<EOT>> /etc/crontab
